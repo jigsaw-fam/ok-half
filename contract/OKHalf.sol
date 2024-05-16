f4f236d581891444eb5617c25bd067753e06ea07
diewland <diewland@gmail.com>
2024-05-16 20:56:02 +0700

    rewrite contract

--- contract/OKHalf.sol
+++ contract/OKHalf.sol
@@ -1,32 +1,36 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC1155/presets/ERC1155PresetMinterPauser.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
-contract OKHalf is ERC1155PresetMinterPauser {
+contract OKHalf is ERC1155, Ownable {
+    constructor(address initialOwner)
+        ERC1155("https://jigsaw-fam.github.io/ok-half/json/")
+        Ownable(initialOwner)
+    {}
 
-    string public name = "OK Half";
-    string public symbol = "OKHALF";
-    string private baseURI = "https://jigsaw-fam.github.io/ok-half/json/";
-
-    constructor() ERC1155PresetMinterPauser("") {
-        _mint(msg.sender, 1, 1, ""); // Chronicle #329 | MXVE ON
+    // metadata
+    function setURI(string memory newuri) public onlyOwner {
+        _setURI(newuri);
+    }
+    function tokenURI(uint tokenId) public view returns (string memory) {
+        return string.concat(uri(0), Strings.toString(tokenId), ".json");
     }
 
     // mint
-    function adminMint(uint256 id, uint256 amount) public virtual {
-        require(hasRole(DEFAULT_ADMIN_ROLE, _msgSender()), "admin only");
-        mint(_msgSender(), id, amount, "");
+    function mint(address account, uint256 id, uint256 amount, bytes memory data)
+        public
+        onlyOwner
+    {
+        _mint(account, id, amount, data);
     }
-
-    // metadata
-    function setBaseURI(string calldata _newBaseURI) external {
-        require(hasRole(DEFAULT_ADMIN_ROLE, _msgSender()), "admin only");
-        baseURI = _newBaseURI;
+    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
+        public
+        onlyOwner
+    {
+        _mintBatch(to, ids, amounts, data);
     }
-    function tokenURI(uint tokenId) public view returns (string memory) {
-        return string.concat(baseURI, Strings.toString(tokenId), ".json");
-    }
-
 }
